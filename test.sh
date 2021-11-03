4a324f0269885ea93ad83499211bde616f4e6290
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-11-04 04:40:07 +0900

    add shebang

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+commit_hash_list=(`git log --pretty=%H`);
+index=1;
+git diff ${commit_hash_list[$index]} --name-only
+git diff ${commit_hash_list[$index]}
+echo ${commit_hash_list[$index]};
