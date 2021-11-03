8802000b2be10c95f6c29415a62917b9a89f3fc0
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-11-04 04:58:42 +0900

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 commit_hash_list=(`git log --pretty=%H`);
 index=1;
-git diff ${commit_hash_list[$index]} --name-only | xargs echo
+file_list=(`git diff ${commit_hash_list[$index]} --name-only`)
+echo $file_list
 # git diff ${commit_hash_list[$index]}
 # echo ${commit_hash_list[$index]};
