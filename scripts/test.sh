8701d94a280869833e8f5989bf4dca01cfd429da
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-11-04 04:56:18 +0900

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 commit_hash_list=(`git log --pretty=%H`);
 index=1;
-git diff ${commit_hash_list[$index]} --name-only
+git diff ${commit_hash_list[$index]} --name-only | xargs echo
 # git diff ${commit_hash_list[$index]}
 # echo ${commit_hash_list[$index]};
-echo hoge
