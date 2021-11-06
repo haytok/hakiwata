c4f78abbb89e7bb8c32984edf805e28a26c5bd1e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-11-07 04:19:39 +0900

    delete an unused file

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-commit_hash_list=(`git log --pretty=%H`);
-index=1;
-file_list=(`git diff ${commit_hash_list[$index]} --name-only`);
-echo $file_list;
-echo hoge;
-# git diff ${commit_hash_list[$index]}
-# echo ${commit_hash_list[$index]};
